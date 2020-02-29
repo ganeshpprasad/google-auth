788f373d6a2efc2be03cbfcc2321e076202f07e1
ganeshp <ganeshpprasad@gmail.com>
2020-02-29 20:03:27 +0530

    server ported

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
+const dev = process.env.NODE_ENV !== "production";
+
 const app = express();
+app.set("port", process.env.PORT || 3330);
 
 const verifyToken = (req, res, next) => {
 	const bearerHeader = req.headers["authorisation"];
